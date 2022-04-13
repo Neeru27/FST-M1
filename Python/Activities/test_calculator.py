cdbf7d1dceb3ebd542f3f0781b7e8a47cadfe76c
Neeru27 <103576349+Neeru27@users.noreply.github.com>
2022-04-13 23:36:30 +0530

    FST - Batch 33

---
+++ Python/Activities/test_calculator.py
@@ -0,0 +1,53 @@
+import pytest
+
+
+def test_addition():
+	
+    
+	num1 = 19
+	num2 = 16
+    
+	
+	sum = num1 + num2
+
+	
+	assert sum == 35
+
+
+def test_subtraction():
+  
+	
+	num1 = 51
+	num2 = 36
+    
+	
+	diff = num1 - num2
+
+	
+	assert diff == 15
+
+
+def test_multiplication():
+  
+	
+	num1 = 9
+	num2 = 20
+    
+	
+	prod = num1 * num2
+
+	
+	assert prod == 180
+
+
+def test_division():
+  
+	
+	num1 = 100
+	num2 = 50
+    
+	
+	quot = num1 / num2
+
+	
+	assert quot == 2
